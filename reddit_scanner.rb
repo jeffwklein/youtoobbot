e2373eadcfff4982fd010e4dbaffaaad2e533537
Jeffrey Klein <jeffwklein@gmail.com>
2014-04-13 22:57:30 -0400

    Youtube access working. starting on comment formatting"

--- reddit_scanner.rb
+++ reddit_scanner.rb
@@ -4,6 +4,7 @@
 # redditkit is an API wrapper for Reddit written in Ruby
 require 'redditkit'
 require './youtube_access'
+require './comment_generator'
 
 ###
 
@@ -14,11 +15,11 @@ password = "csci4330"
 
 # sign in to bot's account
 RedditKit.sign_in username, password
-# build suspense (delay before next request actually)
 sleep 2
+print "\n\n"
 
-# establish time between refreshing feed. default 30
-minutes_to_wait = 30
+# establish time in minutes between requests
+minutes_to_wait = (1/6)  # 10 seconds
 minutes_to_wait = ARGV[1].to_i if ARGV[1]
 
 # notify and exit on login failure
@@ -44,19 +45,19 @@ end
 sleep 2
 
 # save system time for reference point
-last_time = Time.new
+initial_time = last_time = Time.new
 
-# initialize variables for loop.
-links_list = []
+# initialize array of 50 nil values for consistent size (nils will be pushed out eventually)
 already_viewed = Array.new 50, nil
-before_id = nil
 counter = 0
+new_links_count = 0
 
 # repeat loop, wait for some time, repeat
 while true
-  puts "#{ counter+=1 } iterations. #{ Time.new - last_time } seconds passed."
+  # wait before making next request
   # sleep( 60 * minutes_to_wait )
   sleep 10
+  puts "\n#{ counter+=1 } iterations. #{ Time.new - last_time - 10 } seconds passed."
   last_time = Time.new
 
   # retrieve new posts up until first instance of last group of posts retrieved
@@ -64,24 +65,31 @@ while true
 
   # remove link from list if it's already been viewed
   links_list.delete_if { |link| already_viewed.include? link }
+  links_list.keep_if { |link| link.domain == "youtube.com" || link.domain == "youtu.be" }
+  new_links_count += links_list.size if counter > 1
 
-  puts "Size of links list: #{links_list.size}"
-  puts "\t.. First: #{already_viewed.first.title}" unless already_viewed.first.nil?
-  puts "\t.. Last: #{already_viewed.last.title}" unless already_viewed.last.nil?
-  puts "\t.. Size: #{already_viewed.size}"
+  puts "\tSize of links list: #{links_list.size}"
+  puts "\t#{new_links_count} new links discovered in #{Time.new - initial_time} seconds"
+  puts "\tSize of already viewed links list: #{already_viewed.size}"
+  puts "\tFirst on already viewed links list: #{already_viewed.first.title}" unless already_viewed.first.nil?
+  puts "\tLast on already viewed links list: #{already_viewed.last.title}" unless already_viewed.last.nil?
 
   # iterate through newly retrieved links
-  links_list.reverse.each do |link|
-    if link.domain == "youtube.com" || link.domain == "youtu.be"
-      # some stuff..
-    end
-    already_viewed.shift
-    already_viewed << link
-    puts "\t#{link.domain}\t#{link.title}"
+  puts "\tNew links found: #{links_list.size}"
+  links_list.reverse_each do |link|
+    # send to youtube_access.rb, use URL to get data and return as hash
+    # send hash to comment_generator.rb (or method here) to format the data into comment string
+    # finally post comment to link's comments section on reddit
+    already_viewed.pop
+    already_viewed.unshift link
+    puts "\t\t#{link.domain}\t#{link.title}"
   end
 end
 
 
+###
+# pseudocode / notes
+#
 # retrieve videos:
 # videos_list = RedditKit.links(vids, { limit: x, time: 
 # sleep 2 between requests
--- youtube_access.rb
+++ youtube_access.rb
@@ -1,2 +1,53 @@
 # youtube_access.rb
 # Authors: Jeffrey Klein and James Smith
+
+require 'google/api_client'
+#require 'oauth/oauth_util'
+
+class YouTubeAccess 
+  attr_reader :access
+
+  # initialize client
+  def initialize
+    #auth_util = CommandLineOAuthHelper.new("https://www.googleapis.com/auth/youtube.readonly")
+    #client.authorization = auth_util.authorize()
+
+    @client = Google::APIClient.new(
+      key: "AIzaSyCO3kxkYcvX9IIT8kcDjJqMDunN9tZqwxQ",
+      authorization: nil,
+      application_name: "YouToobBot",
+      application_version: "0.0.0"
+    )
+    # access api
+    @access = @client.discovered_api("youtube", "v3")
+    # verify connection
+    unless @access.nil?
+      puts "Established connection to YouTube API" 
+    else
+      puts "Unable to establish connection to YouTube API" 
+      exit
+    end
+  end
+
+  # get info about video using YouTube API
+  def get_video_info(video_url)
+    video_id = isolate_id video_url
+    response = @client.execute!(
+      api_method: @access.videos.list,
+      parameters: {
+        part: "snippet,contentDetails,statistics",
+        id: video_id
+      }
+    )
+    response
+  end
+
+  private
+
+  # helper method to isolate unique video id from URL
+  def isolate_id(video_url)
+    video_id = video_url[/[\?&]v=.{11}/]
+    video_id = video_id[3..-1]
+  end
+
+end

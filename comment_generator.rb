be6966352cc5d340d1af8ee18b770f33e54c2ece
Jeffrey Klein <jeffwklein@gmail.com>
2014-04-14 01:12:37 -0400

    Comment formatting up and working

--- comment_generator.rb
+++ comment_generator.rb
@@ -5,7 +5,7 @@
 require 'pp'
 
 module CommentGenerator
-  def self.generate_comment(response_object, video_url=nil)
+  def self.generate_comment(response_object, video_url = nil)
     video = response_object.data.items[0]
     return nil if video.nil?
     begin
@@ -16,56 +16,47 @@ module CommentGenerator
         publish_time: video.snippet.published_at.strftime("%I:%M%p"),
         channel_title: video.snippet.channel_title,
         channel_url: "https://www.youtube.com/channel/#{video.snippet.channel_id}",
-        description: video.snippet.description[0...400],
+        description: video.snippet.description[0...1000],
         thumbnail_url: video.snippet.thumbnails.high.url,
-        duration: video.contentDetails.durationdur.gsub(/[PTMS]/, "PT" => "", "M" => ":", "S" => ""),
+        duration: video.contentDetails.duration.gsub(/[PTMS]/, "PT" => "", "M" => ":", "S" => ""),
         view_count: video.statistics.view_count,
         like_count: video.statistics.like_count,
         dislike_count: video.statistics.dislike_count,
         favorite_count: video.statistics.favorite_count,
-        comment_count: video.statistics.comment_count
       }
     rescue NoMethodError => err
       puts "Error accessing attribute \"#{err.name}\""
       return nil
     end
     pp data
-    format_comment data
+    return format_comment data
   end
 
   private
 
   def self.format_comment(data)
     return nil if data.nil?
-    comment = "#*^some ^brief ^info ^about ^this ^video:*\n\n"
-    # title
-    comment += "###[#{data[:title]}](#{data[:url]})" if data[:title] && data[:url]
-    # thumbnail preview
-    comment += "[^^^\\[thumbnail ^^^preview\\]](#{data[:thumbnail_url]})\n\n" if data[:thumbnail_url]
-    comment += "\n\n" unless data[:thumbnail_url]
-    # upload and channel info
+    comment = "#*^^some ^^brief ^^info ^^about ^^this ^^video:*\n\n"
+    # title and thumbnail preview
+    comment += "###[#{data[:title]}](#{data[:url]})"
+    comment += "[*^^^\\[thumbnail ^^^preview\\]*](#{data[:thumbnail_url]})\n\n"
+    # upload time and channel info
+    comment += "#*^Uploaded ^on ^#{data[:publish_date]} ^at ^#{data[:publish_time]} "
+    comment += "^to ^channel:* [**^#{data[:channel_title].split.join(" ^")}**](#{data[:channel_url]})\n\n"
+    # duration, views, like %, favorites
+    comment += "*Duration:* **#{data[:duration]}**\n\n"
+    comment += "# *Views:* **#{data[:view_count].to_s.reverse.scan(/\d{1,3}/).join(",").reverse}**\n\n"
+    if (data[:like_count] + data[:dislike_count]) > 0
+      comment += "# **#{((data[:like_count].to_f/(data[:like_count]+data[:dislike_count]))*100).to_i}%** "
+      comment += "*of YouTube voters liked this video, with* **#{data[:favorite_count]}** *favorites*\n\n"
+    else
+      comment += "# *No one has liked or disliked this video on YouTube yet*\n\n"
+    end
+    # video description
+    comment += "*Video Description:*\n"
+    comment += ">#{data[:description].split("\n").join("\n>\n>")}"
+    return comment
   end
 
 end
-
-#     #*^some ^brief ^info ^about ^this ^video:*
-#
-#     ### [Drug Wars: Cannabis Vs  Nicotine](https://www.youtube.com/watch?v=yXaCZasVtZk)[^^^\[thumbnail ^^^preview\]](http://www.google.com)
-#
-#     #*^Uploaded ^on ^03/21/2014 ^at ^04:52AM ^to ^channel:* [**^Christopher ^Pool**](https://www.youtube.com/channel/UCsBPXkfF3J3uCRzl012r8dA)
-#
-#     # *Duration:* **2:13**
-#
-#     # *Views:* **67,745**
-#
-#     # **82%** *of YouTube voters liked this video, with*  **0** *favorites.*
-#
-#     *Video Description:*
-#     >ChrisPoool.com
-#     >
-#     >https://twitter.com/ChrisPoool
-#     >
-#     >Music - Dark Fog by Kevin MacLeod
-#
-
 

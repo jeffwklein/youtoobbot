22be3778b98a4979986ee613a0b820be4c706eaf
Jeffrey Klein <jeffwklein@gmail.com>
2014-05-02 14:15:39 -0400

    Added stop word removal

--- video_data.rb
+++ video_data.rb
@@ -1,12 +1,19 @@
 # video_data.rb
 # Authors: Jeffrey Klein and James Smith
 #
+# Class that represents data about a particular YouTube
+# video posted to reddit.com. The class extracts video
+# information from the YouTube video page's source code.
+# Then it formats extracted tags into a search-friendly
+# format, removing stop words and stemming.
+#
 require 'open-uri'
 require 'redditkit'
 require 'fast-stemmer'
+require './stop_words'
 
 class VideoData
-  attr_reader :url, :title, :post_url, :date, :pic, :keywords, :views, :votes, :stems
+  attr_reader :url, :title, :post_url, :date, :pic, :keywords, :views, :votes
 
   def initialize(reddit_post)
     @post_url = reddit_post.permalink
@@ -25,17 +32,18 @@ class VideoData
     @views = safe_match(/<span class="watch-view-count " >\n *(.*?)\n/, source_str).gsub(/,/,"")
     @views = 0 if @views == ""
     # tags, category, vid title; all stemmed.
-    @stems = safe_match(/"keywords": "(.*?)",/, source_str)
-    @stems = format_stems(@stems)
+    @keywords = safe_match(/"keywords": "(.*?)",/, source_str)
+    @keywords = remove_chars(@keywords)
     category = safe_match(/<p id="eow-category"><a.*?>(.*?)<\/a>/, source_str)
-    if (add_category = format_stems(category)) != nil
-      @stems += add_category
+    if (add_category = remove_chars(category)) != nil
+      @keywords += add_category
     end
-    if (add_title = format_stems(@title)) != nil
-      @stems += add_title
+    if (add_title = remove_chars(@title)) != nil
+      @keywords += add_title
     end
-    @stems.uniq!
-    @keywords = @stems.map { |word| Stemmer::stem_word(word) }.sort.join(" ")
+    @keywords.uniq!
+    @keywords.delete_if { |word| StopWords.is_stop_word?(word) }
+    @keywords = @keywords.map { |word| Stemmer::stem_word(word) }.sort.join(" ").strip
   end
 
   private
@@ -49,8 +57,7 @@ class VideoData
   end
 
   # formats given string into standardized array format. nil if nil value provided
-  def format_stems(str)
+  def remove_chars(str)
     return str.gsub(/,/, " ").gsub(/[^\w\d ]/,"").downcase.split
   end
-
 end
